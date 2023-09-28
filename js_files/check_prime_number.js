48f7d4e2df718c0fdc0262a0bc94abf32e9794fb
selva2git <100025999+selva2git@users.noreply.github.com>
2023-09-28 19:36:56 +0530

    Update check_prime_number.js
    Modified the file

--- js_files/check_prime_number.js
+++ js_files/check_prime_number.js
@@ -2,7 +2,7 @@
 
 // take input from the user
 const number = parseInt(prompt("Enter a positive number: "));
-let isPrime = true;
+let isPrime = true ;
 
 // check if number is equal to 1
 if (number === 1) {
@@ -10,13 +10,14 @@ if (number === 1) {
 }
 
 // check if number is greater than 1
-else if (number > 1) {
+else if (number > 1) 
+{
 
     // looping through 2 to number-1
     for (let i = 2; i < number; i++) {
         if (number % i == 0) {
-            isPrime = false;
-            break;
+            isPrime = false ;
+                break ;
         }
     }
 
