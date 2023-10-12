b780f0a1867c2282a6f4b051251d80df86fba2ab
selva2git <100025999+selva2git@users.noreply.github.com>
2023-10-12 21:44:05 +0530

    Update js_lint_sample.js

--- js_files/js_lint_sample.js
+++ js_files/js_lint_sample.js
@@ -1,7 +1,8 @@
 // Function is called, the return value will end up in x
-let x = myFunction(4, 5);
+let x = myFunction(4, 5)   ;
 
-function myFunction(a, b) {
+function myFunction(a, b) 
+    {
 // Function returns the product of a and b
-	return a * b;
+	return      a      * b;
 }
