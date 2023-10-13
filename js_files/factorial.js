50fc770006f4fef5482bb76359799f8acc559f18
selva2git <100025999+selva2git@users.noreply.github.com>
2023-10-13 17:51:33 +0530

    Update factorial.js

--- js_files/factorial.js
+++ js_files/factorial.js
@@ -1,11 +1,11 @@
 // program to find the factorial of a  number
 
 // take input from the user
-const number =   parseInt(prompt(  "Enter a positive integer:  ") );
+const number =   parseInt(prompt(  "Enter a positive integer:  "));
 
 // checking if number is negative
-if (number <     0)    {
-	console.log("Error! Factorial for negative number does not exist.")   ;
+if (number <     0){
+	console.log("Error! Factorial for negative number does not exist.");
 }
 
 // if number is 0
@@ -14,7 +14,7 @@ else     if (number ===  0)
 
 
 {
-	console.log(`The factorial of ${number} is 1.`)      ;
+	console.log(`The factorial of ${number} is 1.`);
 }
 
 // if number is positive
